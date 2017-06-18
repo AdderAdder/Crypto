32751390e43136543ccfe35a18bbfa8266114d96
Johannes Westlund <johannes.wirkkala.westlund@gmail.com>
2017-06-18 21:54:42 +0200

    Minor changes to generatePrime function

--- crypto.hs
+++ crypto.hs
@@ -2,55 +2,32 @@ import qualified System.Environment as Sys
 import qualified Data.List as List
 import qualified System.Random as Random
 import qualified Data.Bits as Bits
--- Just only for debugging purpose, remove in final version
+-- Used for debugging purpose, remove in final version.
 -- To print binary representation use command: showIntAtBase 2 intToDigit number ""
 import Numeric (showIntAtBase)
 import Data.Char (intToDigit)
 
+-- Datastructure to hold if we are encrypting or decrypting
+data Mode = Encrypt | Decrypt deriving (Read)
+
 main = do args <- Sys.getArgs
           process args
 
+-- Process the command line arguments and takes appropriate action for each scenario.
 process :: [String] -> IO()
-process (fileName:[]) = do file <- (readFile fileName)
-                           randP <- Random.randomRIO (10,1000)
-                           randQ <- Random.randomRIO (10,1000)
-                           seedE <- Random.newStdGen
-                           let [p,q,e] = generateKey randP randQ seedE
-                           writeFile "key.txt" $ (show p) ++ " " ++ (show q) ++ " " ++ (show e)
-                           rsa file [p,q,e]
-process (fileName:p:q:e:[]) = do file <- (readFile fileName)
-                                 rsa file $ (read p :: Integer):(read q :: Integer):(read e :: Integer):[]
+process (mode:fileName:[]) = do file <- (readFile fileName)
+                                seedFirstPrime <- Random.newStdGen
+                                seedSecondPrime <- Random.newStdGen
+                                let p = generatePrime 42 seedFirstPrime
+                                rsa (read mode) file (p) 5
+process (mode:fileName:n:c:[]) = do file <- (readFile fileName)
+                                    rsa (read mode) file (read n :: Integer) (read c :: Integer)
 process _ = putStrLn "Error when parsing argument.\nPlease enter filepath to file that you wish to encrypt.\nOptionally the key to use for encryption can also be specified on format 'fileToEncrypt p q e'."
 
-rsa :: String -> [Integer] -> IO()
-rsa content (p:q:e:[]) = print "Done"
+rsa :: Mode -> String -> Integer -> Integer -> IO()
+rsa mode content n c = print "Done"
 
-generateKey :: Integer -> Integer -> Random.StdGen -> [Integer]
-generateKey randP randQ seedE = [p,q,e]
-                                where
-                                p = getPrime randP
-                                q = getPrime randQ
-                                e = generateE ((p-1)*(q-1)) seedE 0
-
-generateE :: Integer -> Random.StdGen -> Integer -> Integer
-generateE phi seedE e
-  | gcd e phi == 1 = e
-  |otherwise = generateE phi newSeed newE
-               where
-               (newE,newSeed) = Random.randomR (3,phi-1) seedE
-
--- Modified implementation of the simple sieve of Eratosthenes for finding primes
--- based on the implementation given at https://wiki.haskell.org/Prime_numbers
--- To be changed to a non-deterministic prime generator using the Miller-Rabbit method.
-getPrime :: Integer -> Integer
-getPrime n
-  | n < 1 = error "Input has to be larger than 0."
-  | n == 1 = 2
-  | otherwise = sieve n [3,5..10000]
-                where
-                sieve 2 (x:xs) = x
-                sieve m (x:xs) = sieve (m-1) (xs List.\\ [x,x+x..10000])
-
+-- Naive prime checking. Should be changed to the Miller-Rabbit function instead.
 isPrime :: Integer -> Bool
 isPrime number
   | mod number 2 == 0 = (number == 2)
@@ -59,20 +36,28 @@ isPrime number
     where
     sqrtNumber = 1 + (truncate (sqrt (fromIntegral number)))
 
-generatePrime :: Int -> Random.StdGen -> Maybe Integer
+-- Try to generate a prime of b bits using a random seed.
+generatePrime :: Int -> Random.StdGen -> Integer
 generatePrime b seed =
   let number = generateBitNumber b seed
       searchResult = searchForPrime number (2*b)
-  in if isPrime number then Just number
-     else if searchResult /= Nothing then searchResult
-     else Nothing
+      newSeed = generateNewSeed seed b
+      generateNewSeed s 0 = s
+      generateNewSeed s iter = generateNewSeed (snd (Random.random s)) (iter-1)
+  in if isPrime number then number
+     else if searchResult /= Nothing then (\(Just x) -> x) searchResult
+     else generatePrime b newSeed
 
+-- Generates a random number of b bits by using a random seed.
 generateBitNumber :: Int -> Random.StdGen -> Integer
 generateBitNumber b seed = (Bits.setBit num 0) :: Integer
                  where
                  randPos = map (\x -> mod x b) (take b (Random.randoms seed))
                  num = foldl (\bits pos -> Bits.setBit bits pos) (Bits.bit (b-1)) randPos
 
+-- Searches if there is a prime close to the 'number' argument.
+-- Note that the 'iteration' argument should not be too large as searching is a
+-- expensive procedure.
 searchForPrime :: Integer -> Int -> Maybe Integer
 searchForPrime _ 0 = Nothing
 searchForPrime number iteration
