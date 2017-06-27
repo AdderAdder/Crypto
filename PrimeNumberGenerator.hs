13c19ec3a75ed8acda844c6a24985d558c7dd512
Johannes Westlund <johannes.wirkkala.westlund@gmail.com>
2017-06-27 23:14:52 +0200

    Miller-Rabin primality test implemented

--- PrimeNumberGenerator.hs
+++ PrimeNumberGenerator.hs
@@ -1,24 +1,15 @@
-module PrimeNumberGenerator (generatePrime) where
+module PrimeNumberGenerator (generatePrime,powMod) where
 
 import qualified System.Random as Random
 import qualified Data.Bits as Bits
-
--- Naive prime checking. Should be changed to the Rabin-Miller function instead.
-isPrime :: Integer -> Bool
-isPrime number
-  | mod number 2 == 0 = (number == 2)
-  | mod number 3 == 0 = (number == 3)
-  | otherwise = foldl (\prime val -> if (mod number (val-1) == 0) || (mod number (val+1) == 0) then prime && False else prime && True) True [6,12..sqrtNumber]
-    where
-    sqrtNumber = 1 + (truncate (sqrt (fromIntegral number)))
 
 -- Try to generate a prime of b bits using a random seed.
 generatePrime :: Int -> Random.StdGen -> Integer
 generatePrime b seed =
   let number = generateBitNumber b seed
-      searchResult = searchForPrime number (2*b)
+      searchResult = searchForPrime number (2*b) seed
       newSeed = Random.mkStdGen (fst (Random.random seed))
-  in if isPrime number then number
+  in if isPrime number 100 seed then number
      else if searchResult /= Nothing then (\(Just x) -> x) searchResult
      else generatePrime b newSeed
 
@@ -32,8 +23,31 @@ generateBitNumber b seed = (Bits.setBit num 0) :: Integer
 -- Searches if there is a prime close to the 'number' argument.
 -- Note that the 'iteration' argument should not be too large as searching is an
 -- expensive procedure.
-searchForPrime :: Integer -> Int -> Maybe Integer
-searchForPrime _ 0 = Nothing
-searchForPrime number iteration
-  | isPrime (number+2) = Just (number+2)
-  | otherwise = searchForPrime (number+2) (iteration-1)
+searchForPrime :: Integer -> Int -> Random.StdGen -> Maybe Integer
+searchForPrime _ 0 _ = Nothing
+searchForPrime number iteration seed
+  | isPrime (number+2) 100 seed = Just (number+2)
+  | otherwise = searchForPrime (number+2) (iteration-1) seed
+
+-- Preformes mod (num**exp) n but faster through repeated squaring.
+powMod :: Integer -> Integer -> Integer -> Integer
+powMod _ 0 _ = 1
+powMod num exp n = if mod exp 2 == 0 then rec else mod (num*rec) n
+                  where rec = powMod (mod (num*num) n) (div exp 2) n
+
+-- Miller-Rabin primality test based on pseudocode found at https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test
+isPrime :: Integer -> Int -> Random.StdGen -> Bool
+isPrime num k seed = foldl (\acc val -> if (powMod val d num) == 1 || (powMod val d num) == (num-1)
+                    then True && acc else (check (powMod val d num) (r-1) num) && acc) True randNums
+                    where
+                    randNums = map (\x -> 2 + (mod x (num-4))) (take k (Random.randoms seed))
+                    (r,d) = factor (num-1) 0
+                    factor :: Integer -> Integer -> (Integer,Integer)
+                    factor x r = if mod x 2 == 0 then factor (div x 2) (r+1) else (r,x)
+                    check :: Integer -> Integer -> Integer -> Bool
+                    check _ (-1) _ = False -- Needed if r = 0 as (r-1) is the -1. This happens for example if num = 22
+                    check _ 0 _ = False
+                    check x times n = let xx = mod (x*x) n
+                                      in if xx == 1 then False
+                                      else if xx == (n-1) then True
+                                      else check xx (times-1) n
